ebff54944cc0d3d5a0a387c6895f25e9589efcab
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2024-11-09 05:47:06 +0000

    DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,27 +20,29 @@ class DatabaseSeeder extends Seeder
         // Comment::truncate();
         // Blog::truncate();
         // Category::truncate();
-        // $mgmg = User::factory()->create(['name' => 'mg mg', 'username' => 'mg-mg']);
-        // $aung = User::factory()->create(['name' => 'aung aung', 'username' => 'aung-aung']);
+        $mgmg = User::factory()->create(['name' => 'mg mg', 'username' => 'mg-mg']);
+        $aung = User::factory()->create(['name' => 'aung aung', 'username' => 'aung-aung']);
 
-        // $category1 = Category::factory()->create(['name' => 'Category 1', 'slug' => 'category-1']);
-        // $category2 = Category::factory()->create(['name' => 'Category 2', 'slug' => 'category-2']);
+        User::factory()->create(['name' => 'azono', 'username' => 'azono', 'email' => 'azono@gmail.com', 'password' => 'password']);
+
+        $category1 = Category::factory()->create(['name' => 'Category 1', 'slug' => 'category-1']);
+        $category2 = Category::factory()->create(['name' => 'Category 2', 'slug' => 'category-2']);
 
         /**
          * ! Faker Max unique value = 182
          */
         $blog1 = Blog::factory(100)->create();
-        // $blog2 = Blog::factory()->create(['category_id' => $category1->id, 'user_id' => $aung->id]);
-        // $blog3 = Blog::factory()->create(['category_id' => $category2->id, 'user_id' => $aung->id]);
-        // $blog4 = Blog::factory()->create(['category_id' => $category2->id, 'user_id' => $mgmg->id]);
+        $blog2 = Blog::factory()->create(['category_id' => $category1->id, 'user_id' => $aung->id]);
+        $blog3 = Blog::factory()->create(['category_id' => $category2->id, 'user_id' => $aung->id]);
+        $blog4 = Blog::factory()->create(['category_id' => $category2->id, 'user_id' => $mgmg->id]);
 
-        // Comment::factory(2)->create(['blog_id' => $blog1->id, 'user_id' => $mgmg->id]);
-        // Comment::factory(2)->create(['blog_id' => $blog1->id, 'user_id' => $aung->id]);
-        // Comment::factory(2)->create(['blog_id' => $blog2->id, 'user_id' => $mgmg->id]);
-        // Comment::factory(2)->create(['blog_id' => $blog2->id, 'user_id' => $aung->id]);
-        // Comment::factory(2)->create(['blog_id' => $blog3->id, 'user_id' => $mgmg->id]);
-        // Comment::factory(2)->create(['blog_id' => $blog3->id, 'user_id' => $aung->id]);
-        // Comment::factory(2)->create(['blog_id' => $blog4->id, 'user_id' => $mgmg->id]);
-        // Comment::factory(2)->create(['blog_id' => $blog4->id, 'user_id' => $aung->id]);
+        Comment::factory(2)->create(['blog_id' => $blog1[1]->id, 'user_id' => $mgmg->id]);
+        Comment::factory(2)->create(['blog_id' => $blog1[1]->id, 'user_id' => $aung->id]);
+        Comment::factory(2)->create(['blog_id' => $blog2->id, 'user_id' => $mgmg->id]);
+        Comment::factory(2)->create(['blog_id' => $blog2->id, 'user_id' => $aung->id]);
+        Comment::factory(2)->create(['blog_id' => $blog3->id, 'user_id' => $mgmg->id]);
+        Comment::factory(2)->create(['blog_id' => $blog3->id, 'user_id' => $aung->id]);
+        Comment::factory(2)->create(['blog_id' => $blog4->id, 'user_id' => $mgmg->id]);
+        Comment::factory(2)->create(['blog_id' => $blog4->id, 'user_id' => $aung->id]);
     }
 }
